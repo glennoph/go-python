f12bb04410c07917c62f0d84490db072b35b78d4
glenn opdycke-hansen <glennoph@gmail.com>
2022-11-29 10:05:59 -0600

    Create exercise_1.py

---
+++ d02/exercise_1.py
@@ -0,0 +1,5 @@
+# Create a program that prompts the user to input their name once.
+# Then, the program prints out the name once with the first letter capitalized.
+
+name = input('enter your name: ')
+print(name.capitalize())
