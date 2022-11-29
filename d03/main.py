3e630dcf897eb1370d8f4a655572aee9a059ec05
glenn opdycke-hansen <glennoph@gmail.com>
2022-11-29 10:09:11 -0600

    Create main.py

---
+++ d03/main.py
@@ -0,0 +1,30 @@
+# input user action
+
+todos = []
+
+while True:
+    user_action = input('enter add, show, edit, or exit: ')
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input('enter a todo: ')
+            todos.append(todo) # add todo to list
+        case 'show':
+            i =1
+            for item in todos:
+                print(i,item.capitalize())
+                i = i+1
+
+        case 'edit':
+            item = input('enter item number: ')
+            print(item, todos[int(item)-1])
+            new_item = input('enter new item todo: ')
+            todos[int(item)-1] = new_item
+
+        case 'exit':
+            break
+        case _:
+            print('unknown action')
+
+print('bye')
